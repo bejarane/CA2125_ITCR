37da728585ebfe4a23806c5973a52140e8a92bd8
Emmanuel <39421808+bejarane@users.noreply.github.com>
2021-04-26 14:56:51 -0600

    new exam

--- EXAMS/project_phone_guide/phone_guide.py
+++ EXAMS/project_phone_guide/phone_guide.py
@@ -6,6 +6,8 @@ filedata= open(filename,'r')
 
 datalines = filedata.readlines()
 
+filedata.close()
+
 captureddata = []
 
 for line in datalines:
@@ -13,9 +15,13 @@ for line in datalines:
     captureddata += re.findall("[^\(\-\d](\d{3})[^\)\-\d]",line)
 
 print("Results")
+fileoutput = open("output.txt",'w+')
 for number in captureddata:
+    fileoutput.write(number)
+    fileoutput.write("\n")
     print(number)
 print()
 total = len(captureddata)
 print(f'Total phone numbers found: {total}')
     
+fileoutput.close()
